c0417a032baac5f9c66f527d06b21962150b719e
Mayur kumar <jgmmkslksmgwn@gmail.com>
2021-07-11 13:31:27 +0530

    first commit

---
+++ Library-Management-System/src/library/management/system/conn.java
@@ -0,0 +1,20 @@
+package library.management.system;
+
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql:///project7","root","GolangDevops");
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
